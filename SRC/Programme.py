faabdac9cc1cb34345c863902a9fb0ddb755b38d
choubael <noutcheuchoubael@gmail.com>
2023-03-05 16:00:38 +0100

    pas encore modif

--- SRC/Programme.py
+++ SRC/Programme.py
@@ -1,8 +1,6 @@
 from pathlib import Path
 from tkinter import Label, StringVar,Tk,Canvas,Text,Button,PhotoImage
 from tkinter.constants import END,WORD
-from sklearn import svm
-import seaborn as sns
 import pickle
 
 #chemin d'accès aux assets
@@ -12,7 +10,7 @@ def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
 window = Tk()
-window.title("TP2 SVM")
+window.title("TP3 FAKE NEWS")
 width = 600 
 height = 300
  
@@ -30,20 +28,14 @@ result=StringVar()
 result.set("------")
 
 #import de notre modele SVM
-with open('mon_model_SVM.pkl', 'rb') as f:
-    model = pickle.load(f)
-    
+with open('PassiveAggressiveClassifier.pkl', 'rb') as f:
+    pac = pickle.load(f)
+with open('TfidfVectorizer.pkl', 'rb') as f:
+    tf = pickle.load(f)
 
-def predire():
-    ok=False
-    try:
-        inputs=[[float(SL.get(0.0,END)),float(SW.get(0.0,END)),float(PL.get(0.0,END)),float(PW.get(0.0,END))]]
-        ok=True
-    except:
-        result.set('Entrer des bonnes Valeurs')
-        ok=False
-    if ok:
-        result.set(model.predict(inputs))
+def analyser():    
+    inputs=[entry.get(0.0,END)]
+    result.set(pac.predict(tf.transform(inputs)))
         
     
 #arrière plan
@@ -66,7 +58,8 @@ bg = canvas.create_image(
     image=bg_image
     )
 
-SL = Text(
+
+entry = Text(
     canvas,
     bd=0,
     bg="#FFFFFF",
@@ -76,65 +69,18 @@ SL = Text(
     cursor="dot",
 
 )
-SL.place(
-    x=181,
+entry.place(
+    x=70,
     y=90,
-    width=90,
-    height=20
+    width=450,
+    height=93
 )
-SW = Text(
-    canvas,
-    bd=0,
-    bg="#FFFFFF",
-    highlightthickness=0,
-    font=("sergio",10,"bold"),
-    wrap=WORD,
-    cursor="dot",
 
-)
-SW.place(
-    x=181,
-    y=160,
-    width=90,
-    height=20
-)
-PL = Text(
-    canvas,
-    bd=0,
-    bg="#FFFFFF",
-    highlightthickness=0,
-    font=("sergio",10,"bold"),
-    wrap=WORD,
-    cursor="dot",
-
-)
-PL.place(
-    x=340,
-    y=90,
-    width=90,
-    height=20
-)
-PW = Text(
-    canvas,
-    bd=0,
-    bg="#FFFFFF",
-    highlightthickness=0,
-    font=("sergio",10,"bold"),
-    wrap=WORD,
-    cursor="dot",
-
-)
-PW.place(
-    x=340,
-    y=160,
-    width=90,
-    height=20
-)
 button_image = PhotoImage(
     file=relative_to_assets("btn.png"))
 button_1 = Button(
     canvas,
-    command=predire,
+    command=analyser,
     image=button_image,
     borderwidth=0,
     highlightthickness=0,
@@ -143,8 +89,8 @@ button_1 = Button(
     bg="#004C7F"
 )
 button_1.place(
-    x=266,
-    y=197,
+    x=260,
+    y=200,
     width=69,
     height=26
 )
@@ -158,7 +104,7 @@ resultat=Label(
 )
 resultat.place(
         x=150,
-        y=260,
+        y=255,
         width=300
 )
 
